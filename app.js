10a4eab3e9bc964b6068e69d13f5e9c9a2d8e902
Tae Nam <taehyunnam@Taes-MacBook-Pro.local>
2020-09-16 19:18:44 -0700

    added __dirname to app.js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var port = process.env.PORT || 8080;
 
 app.set('view engine', 'ejs');
 
-//app.use(express.static(__dirname + '/public'));
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
+//app.use(express.static('public'));
 
 app.get('/', function(req, res) { res.render('index'); });
 
