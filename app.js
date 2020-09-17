c9b4b321884de39c06bcab08cd539905e7bcabf7
Tae Nam <taehyunnam@Taes-MacBook-Pro.local>
2020-09-16 19:08:31 -0700

    changed to node.js from php

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("tae's app.js connected");
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+
+app.set('view engine', 'ejs');
+
+//app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
+
+app.get('/', function(req, res) { res.render('index'); });
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
