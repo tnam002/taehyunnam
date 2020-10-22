28751cd8a1fd5615a5d85326af93b98661879e5a
Tae Nam <taehyunnam@Taes-MacBook-Pro.local>
2020-10-22 11:10:56 -0700

    added ejs to website

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 8080;
+const   express = require('express'),
+        app = express(),
+        port = process.env.PORT || 8080;
 
 app.set('view engine', 'ejs');
-
 app.use(express.static(__dirname + '/public'));
-//app.use(express.static('public'));
 
 app.get('/', function(req, res) { res.render('index'); });
 
